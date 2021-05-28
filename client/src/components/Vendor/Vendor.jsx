6a916277b35c45365fe42d04d7cd7d85a83ac902
jbenet <nycflasher@yahoo.com>
2021-05-28 13:52:53 -0400

    deleted unnecessary code

--- client/src/components/Vendor/Vendor.jsx
+++ client/src/components/Vendor/Vendor.jsx
@@ -9,7 +9,7 @@ const Vendor = (props) => {
             <Link className="vendor" to={`/vendors/${props._id}`}>
                 <img className="vendor-image" src={props.imgURL} alt={props.name} />
                 <div className="vendor-name">{props.name}</div>
-                <div className="hours">{`$${props.hours}`}</div>
+                <div className="hours">{props.hours}</div>
             </Link>
         </>
     )
