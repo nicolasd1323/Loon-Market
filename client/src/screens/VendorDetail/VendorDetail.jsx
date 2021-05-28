914da951f06df7753537cea4aaad12b6123746f4
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-28 10:09:16 -0400

    vendor detail

---
+++ client/src/screens/VendorDetail/VendorDetail.jsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import './VendorDetail.css'
+import { Layout } from '../../components'
+import { getVendor, deleteVendor } from '../../services/vendors'
+import { useParams, Link } from 'react-router-dom'
+
+const VendorDetail = (props) => {
+  const [vendor, setVendor] = useState(null)
+  const [isLoaded, setLoaded] = useState(false)
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchVendor = async () => {
+      const vendor = await getVendor(id)
+      setVendor(vendor)
+      setLoaded(true)
+    }
+    fetchVendor()
+  }, [id])
+
+  if (!isLoaded) {
+    return <h1>Loading...</h1>
+  }
+
+  return (
+    <Layout user={props.user}>
+      <div className='vendor-detail'>
+        <img
+          className='vendor-detail-image'
+          src={vendor.imgURL}
+          alt={vendor.name}
+        />
+        <div className='detail'>
+          <div className='name'>{vendor.name}</div>
+          <div className='description'>{vendor.description}</div>
+          <div className='hours'>{vendor.hours}</div>
+          <div className='button-container'>
+            <Link className='edit-button' to={`/vendor/${vendor._id}/edit`}>
+              Edit
+            </Link>
+            <button
+              className='delete-button'
+              onClick={() => deleteVendor(vendor._id)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default VendorDetail;
