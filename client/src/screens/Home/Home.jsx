35982b83f9d626beeeb2648bc60715936364760c
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-28 11:01:31 -0400

    home4

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,37 +1,15 @@
-import "./Home.css";
-import { Layout, VendorCards } from "../../components";
-import { useState, useEffect } from "react";
-import { getVendors } from "../../services/vendors";
+import './Home.css'
+import VendorCards from '../../components/VendorCards/VendorCards'
+import Layout from '../../components/Layout/Layout'
 
 const Home = (props) => {
-  const [vendors, setVendors] = useState([]);
-
-  useEffect(() => {
-    const fetchVendors = async () => {
-      const aFewVendors = await getVendors();
-      setVendors(aFewVendors);
-    };
-    fetchVendors();
-  }, []);
   return (
     <Layout user={props.user}>
-      <div className="home">
-        {vendors.map((vendor, index) => {    
-          return (
-            index < 3 ? (
-            <VendorCards
-              _id={vendor._id}
-              name={vendor.name}
-              imgURL={vendor.imgURL}
-              key={index}
-            />
-
-            ) : null
-          )
-        })}
+      <div className='home'>
+        <VendorCards />
       </div>
     </Layout>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
