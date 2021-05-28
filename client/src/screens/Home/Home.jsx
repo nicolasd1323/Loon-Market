774cb2f577447746cc079db867e9d6e3b10bfa84
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-28 10:03:11 -0400

    SignIn.jsx VendorCreate/jsx

---
+++ client/src/screens/Home/Home.jsx
@@ -0,0 +1,14 @@
+ort './Home.css'
+import { Layout, VendorCards } from '../../components'
+
+const Home = (props) => {
+  return (
+    <Layout user={props.user}>
+      <div className='home'>
+        <VendorCards />
+      </div>
+    </Layout>
+  )
+}
+
+export default Home
---
+++ client/src/screens/VendorCreate/VendorCreate.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import './VendorCreate.css'
+import { Layout } from '../../components'
+import { Redirect } from 'react-router-dom'
+import { createVendor } from '../../services/vendors'
+
+const VendorCreate = (props) => {
+  const [vendor, setVendor] = useState({
+    name: '',
+    description: '',
+    imgURL: '',
+    price: '',
+  })
+
+  const [isCreated, setCreated] = useState(false)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target
+    setVendor({
+      ...vendor,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    const created = await createVendor(vendor)
+    setCreated({ created })
+  }
+
+  if (isCreated) {
+    return <Redirect to={`/vendors`} />
+  }
+  return (
+    <Layout user={props.user}>
+      <form className='create-form' onSubmit={handleSubmit}>
+        <input
+          className='input-name'
+          placeholder='Name'
+          value={vendor.name}
+          name='name'
+          required
+          autoFocus
+          onChange={handleChange}
+        />
+        <textarea
+          className='textarea-description'
+          rows={10}
+          placeholder='Description'
+          value={vendor.description}
+          name='description'
+          required
+          onChange={handleChange}
+        />
+        <input
+          className='input-image-link'
+          placeholder='Image Link'
+          value={vendor.imgURL}
+          name='imgURL'
+          required
+          onChange={handleChange}
+        />
+        <input
+          className='input-hours'
+          placeholder='Hours'
+          value={vendor.hours}
+          name='hours'
+          required
+          onChange={handleChange}
+        />
+        <button type='submit' className='submit-button'>
+          Submit
+        </button>
+      </form>
+    </Layout>
+  )
+}
+
+export default VendorCreate
