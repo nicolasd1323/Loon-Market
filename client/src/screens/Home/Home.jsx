f2ce4377042a630d3abc069bfef94264a5b92a00
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-28 10:33:20 -0400

    home

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,14 +1,33 @@
-import './Home.css'
-import { Layout, VendorCards } from '../../components'
+import "./Home.css";
+import { Layout, VendorCards } from "../../components";
+import { useState, useEffect } from "react";
+import { getVendors } from "../../services/products";
 
 const Home = (props) => {
+  const [vendors, setVendors] = useState([]);
+
+  useEffect(() => {
+    const fetchVendors = async () => {
+      const aFewVendors = await getVendors();
+      setVendors(aFewVendors);
+    };
+    fetchVendors();
+  }, []);
   return (
     <Layout user={props.user}>
-      <div className='home'>
-        <VendorCards />
+      <div className="home">
+        {vendors.map((vendor) => {
+          return (
+            <VendorCards
+              _id={vendor._id}
+              name={vendor.name}
+              imgURL={vendor.imgURL}
+            />
+          );
+        })}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
