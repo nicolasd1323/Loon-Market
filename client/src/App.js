b5269becef766c9ca23ba8d6b29f35cd8f9c6dc6
Nicolas Salazar <Nico1608@Nicolass-Air.cable.rcn.com>
2021-05-29 13:59:18 -0400

    edit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import './App.css'
 import Home from './screens/Home/Home'
 import Vendors from './screens/Vendors/Vendors'
 import VendorDetail from './screens/VendorDetail/VendorDetail'
-import { Route, Switch } from 'react-router-dom'
+import VendorEdit from './screens/VendorEdit/VendorEdit'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { verifyUser } from './services/users'
 import SignUp from './screens/SignUp/SignUp'
 import SignIn from './screens/SignIn/SignIn'
@@ -41,6 +42,7 @@ const App = () => {
         <Route path="/add-vendor">
         </Route>
         <Route exact path="/vendor/:id/edit">
+          {user ? <VendorEdit user={user} /> : <Redirect to='/' />}
         </Route>
         <Route exact path="/vendors/:id">
           <VendorDetail user={user}/>
