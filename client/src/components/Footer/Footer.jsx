304f493f67e60dceefb1f4272433b1a601d02a3d
jbenet <nycflasher@yahoo.com>
2021-06-03 16:35:29 -0400

    alignment

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ const Footer = () =>
     <div className="footer-container">
       <div className="footer-item-1">
       <div className="footer-address">1234 Loon Cove Dr. Watertown, New Loon </div>
-         <div className="footer-hours">|  Mon - Wed  11 am - 9 pm  |  Thurs - Sat  11 am - 12 am  |  Sun 12 pm - 6 pm</div>  
+         <div className="footer-hours"><b>Mon - Wed</b>  11 am - 9 pm  |  <b>Thurs - Sat</b>  11 am - 12 am  |  <b>Sun</b> 12 pm - 6 pm</div>  
         </div>
       
-      <div className="footer-item-2">&copy; Loon Market  2021  |  <a href="http://endless.horse/">James Benet</a>  |  <a href="http://endless.horse/">Nicolas Salazar</a>  |  <a href="http://endless.horse/">Carri-anne Hamer</a></div>
+      <div className="footer-item-2"><b>&copy; Loon Market  2021</b>  |  <a href="http://endless.horse/">James Benet</a>  |  <a href="http://endless.horse/">Nicolas Salazar</a>  |  <a href="http://endless.horse/">Carri-anne Hamer</a></div>
       
    
    </div>
